4b575312c53057f20018ba100f9e2379d8a85ecf
TUATUI <qq2837175561@gmail.com>
2024-09-05 03:03:43 +0800

    feat: 折叠按钮和限制宽度

--- components/ChatTabs.vue
+++ components/ChatTabs.vue
@@ -3,9 +3,15 @@
     class="flex flex-col h-full tabs-focus"
     @focus="handleClickChatTabs"
     tabindex="0"
+    ref="divElem"
   >
     <div class="flex">
-      <VTabs v-model="data.currTab" bg-color="primary" show-arrows class="grow">
+      <VTabs
+        v-model="data.currTab"
+        bg-color="primary"
+        show-arrows
+        class="grow min-w-0"
+      >
         <VTab :value="i.id" v-for="i in data.topics" :key="i.id">
           {{ i.title }}
           <template #append>
@@ -19,18 +25,46 @@
           </template>
         </VTab>
       </VTabs>
-      <div class="bg-primary">
-        <VBtn
-          icon="mdi-view-split-horizontal"
-          variant="text"
-          @click="$emit('splitHorizontal')"
-        />
-        <VBtn
-          icon="mdi-view-split-vertical"
-          variant="text"
-          @click="$emit('splitVertical')"
-        />
-        <VBtn icon="mdi-close" variant="text" @click="$emit('close')" />
+      <div class="bg-primary flex-shrink-0">
+        <template v-if="viewSize.inlineSize > 300">
+          <VBtn
+            icon="mdi-view-split-vertical"
+            variant="text"
+            @click="splitVertHandle"
+          /><VBtn
+            icon="mdi-view-split-horizontal"
+            variant="text"
+            @click="splitHorizHandle"
+          /><VBtn icon="mdi-close" variant="text" @click="$emit('close')" />
+        </template>
+        <template v-else>
+          <VMenu open-on-hover>
+            <template v-slot:activator="{ props }">
+              <VBtn
+                icon="mdi-dots-vertical"
+                variant="text"
+                v-bind="props"
+              ></VBtn>
+            </template>
+            <VList>
+              <VListItem>
+                <VBtn
+                  icon="mdi-view-split-vertical"
+                  variant="text"
+                  @click="splitVertHandle"
+                /><VBtn
+                  icon="mdi-view-split-horizontal"
+                  variant="text"
+                  @click="splitHorizHandle"
+                /><VBtn
+                  icon="mdi-close"
+                  variant="text"
+                  @click="$emit('close')"
+                />
+              </VListItem>
+            </VList>
+          </VMenu>
+        </template>
       </div>
     </div>
     <VTabsWindow v-model="data.currTab" class="grow flex flex-col [&>*]:grow">
@@ -48,6 +82,7 @@
 <script setup lang="ts">
 const props = defineProps<{ uniqueKey: Symbol }>();
 const store = chatTabsStore();
+const divElem = ref<HTMLDivElement>();
 
 const data =
   store.globalSharedTabs.get(props.uniqueKey) ||
@@ -65,6 +100,7 @@ watch(
     if (newVal > oldVal) data.value.currTab = data.value.topics.at(-1)?.id;
   }
 );
+
 const remove = (topic: TopicData) => {
   const i = data.value.topics.indexOf(topic);
   if (i < 0) return;
@@ -82,13 +118,34 @@ const expose = {
     else data.value.currTab = i;
   },
 };
+
 defineExpose(expose);
 
-defineEmits<{
+const emit = defineEmits<{
   splitHorizontal: [];
   splitVertical: [];
   close: [];
 }>();
+const splitVertHandle = () => {
+  if (!divElem.value) return;
+  if (divElem.value.offsetWidth > 256) emit("splitVertical");
+  else alert("hahhaa");
+};
+
+const splitHorizHandle = () => {
+  if (!divElem.value) return;
+  if (divElem.value.offsetHeight > 256) emit("splitHorizontal");
+  else alert("hahhaa");
+};
+
+const resizeObs = new ResizeObserver((entries) => {
+  const size = entries[0]?.contentBoxSize[0];
+  if (size) viewSize.value = size;
+});
+
+const viewSize = ref<ResizeObserverSize>({ blockSize: 500, inlineSize: 500 });
+onMounted(() => divElem.value && resizeObs.observe(divElem.value));
+onUnmounted(() => resizeObs.disconnect());
 </script>
 <!-- <style lang="css" scoped>
 .tabs-focus:focus{
