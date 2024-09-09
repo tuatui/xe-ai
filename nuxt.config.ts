2e3322481d74a6025753eb6a3205901335dcfa6c
TUATUI <qq2837175561@gmail.com>
2024-09-10 01:10:58 +0800

    fix: 解决了uno-css于vuetify的冲突问题

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  build: { transpile: ["vuetify"] },
   modules: [
-    (_options, nuxt) => {
-      nuxt.hooks.hook("vite:extendConfig", (config) => {
-        config.plugins?.push(vuetify({ autoImport: true }));
-      });
-    },
     "@pinia/nuxt",
     "@unocss/nuxt",
     "@vueuse/nuxt",
+    "vuetify-nuxt-module",
   ],
-  vite: { vue: { template: { transformAssetUrls } } },
-  css: ["~/assets/main.css"],
+  vuetify: {
+    // 手动控制导入，以解决自带的类名和unocss冲突的问题
+    moduleOptions: { disableVuetifyStyles: true },
+  },
+  css: ["~/assets/vuetify/main.scss"],
   ssr: false,
 });
--- plugins/vuetify.ts
+++
@@ -1,8 +0,0 @@
-import "@mdi/font/css/materialdesignicons.css";
-
-import "vuetify/styles";
-import { createVuetify } from "vuetify";
-
-export default defineNuxtPlugin((app) => {
-  app.vueApp.use(createVuetify({ ssr: true }));
-});
