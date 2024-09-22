144019df1e18cb0b2245a2cd9e6c512c2ab6ac80
TUATUI <qq2837175561@gmail.com>
2024-09-23 01:20:09 +0800

    feat: 为vuetify组件增加i18n支持

--- lang/config/i18n.config.ts
+++ lang/config/i18n.config.ts
@@ -1,3 +1,16 @@
+import * as I18nData from "vuetify/locale";
+const { zhHans: zh, ...leftData } = I18nData;
+const Vi18N = { zh, ...leftData };
+
+const messages: Record<string, any> = {};
+
+for (const [key, val] of Object.entries(Vi18N)) {
+  messages[key] = {
+    $vuetify: val,
+  };
+}
+
 export default defineI18nConfig(() => ({
   fallbackLocale: "en",
+  messages,
 }));
