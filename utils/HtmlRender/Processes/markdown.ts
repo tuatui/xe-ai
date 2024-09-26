d19b5f4ef8b34fb323634eabc5eef38be7e42b76
TUATUI <qq2837175561@gmail.com>
2024-09-26 10:09:28 +0800

    feat: 代码高亮

--- utils/HtmlRender/Processes/markdown.ts
+++ utils/HtmlRender/Processes/markdown.ts
@@ -1,3 +1,16 @@
-import { marked } from "marked";
+import { Marked } from "marked";
+import { markedHighlight } from "marked-highlight";
+import hljs from 'highlight.js';
+
+
+const marked = new Marked(
+  markedHighlight({
+    langPrefix: 'hljs language-',
+    highlight(code, lang, info) {
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+      return hljs.highlight(code, { language }).value;
+    }
+  })
+);
 
 export default (text: string) => marked.parse(text);
