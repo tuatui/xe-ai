dea49129850c9ca16d73d0396838bbe96e46af4e
tuatui <qq2837175561@gmail.com>
2024-10-14 18:22:54 +0800

    fix: 减少首屏下载资源

--- utils/HtmlRender/Processes/markdown.ts
+++ utils/HtmlRender/Processes/markdown.ts
@@ -1,34 +1,35 @@
 import { Marked } from "marked";
 import { markedHighlight } from "marked-highlight";
 import hljsCore from "highlight.js/lib/core";
-import plaintext from "highlight.js/lib/languages/plaintext";
-import xml from "highlight.js/lib/languages/xml";
 
-hljsCore.registerLanguage("plaintext", plaintext);
-hljsCore.registerLanguage("xml", xml); // JSX语法需要
 langAliasAddition
   .entries()
   .forEach(([alias, languageName]) =>
     hljsCore.registerAliases([alias], { languageName }),
   );
-
+const analyzeAndImport = async (lang: string) => {
+  const target = langAliasMap.get(lang);
+  if (target !== undefined) {
+    const res = await import(
+      `../../../node_modules/highlight.js/es/languages/${target}.js`
+    );
+    hljsCore.registerLanguage(target, res.default);
+    // JSX语法依赖于XML解析器
+    if (target === "typescript" || target === "javascript") {
+      const res = await import(`highlight.js/lib/languages/xml`);
+      hljsCore.registerLanguage("xml", res.default);
+    }
+  } else if (!hljsCore.getLanguage("plaintext")) {
+    const res = await import(`highlight.js/lib/languages/plaintext`);
+    hljsCore.registerLanguage("plaintext", res.default);
+  }
+};
 const marked = new Marked(
   markedHighlight({
     async: true,
     langPrefix: "hljs language-",
     async highlight(code, lang) {
-      lang = lang.toLowerCase();
-      if (lang && !hljsCore.getLanguage(lang)) {
-        const target = langAliasMap.get(lang);
-        //if (!target) console.log(`missing ${lang}`);
-
-        if (target !== undefined) {
-          const res = await import(
-            `../../../node_modules/highlight.js/es/languages/${target}.js`
-          );
-          hljsCore.registerLanguage(target, res.default);
-        }
-      }
+      await analyzeAndImport(lang);
       const language = hljsCore.getLanguage(lang) ? lang : "plaintext";
       return hljsCore.highlight(code, { language }).value;
     },
