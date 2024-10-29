c58abd693c087e04aefc46592295b736697f247a
tuatui <qq2837175561@gmail.com>
2024-10-29 12:02:11 +0800

    fix: 优化前端语言的高亮显示

---
+++ utils/HtmlRender/Processes/hljs-vue.ts
@@ -0,0 +1,49 @@
+import type { HLJSApi, LanguageFn } from "highlight.js";
+// https://github.com/highlightjs/highlightjs-vue/blob/master/vue.js
+export const hljsDefineVue: LanguageFn = (hljs: HLJSApi) => {
+  return {
+    subLanguage: ["xml", "typescript", "css"],
+    contains: [
+      hljs.COMMENT("<!--", "-->", {
+        relevance: 10,
+      }),
+      {
+        begin: /^(\s*)(<script>)/gm,
+        end: /^(\s*)(<\/script>)/gm,
+        subLanguage: "javascript",
+        excludeBegin: true,
+        excludeEnd: true,
+      },
+      {
+        begin: /^(?:\s*)(?:<script\s+lang=(["'])ts\1>)/gm,
+        end: /^(\s*)(<\/script>)/gm,
+        subLanguage: "typescript",
+        excludeBegin: true,
+        excludeEnd: true,
+      },
+      {
+        begin: /^(\s*)(<style(\s+scoped)?>)/gm,
+        end: /^(\s*)(<\/style>)/gm,
+        subLanguage: "css",
+        excludeBegin: true,
+        excludeEnd: true,
+      },
+      {
+        begin:
+          /^(?:\s*)(?:<style(?:\s+scoped)?\s+lang=(["'])(?:s[ca]ss)\1(?:\s+scoped)?>)/gm,
+        end: /^(\s*)(<\/style>)/gm,
+        subLanguage: "scss",
+        excludeBegin: true,
+        excludeEnd: true,
+      },
+      {
+        begin:
+          /^(?:\s*)(?:<style(?:\s+scoped)?\s+lang=(["'])stylus\1(?:\s+scoped)?>)/gm,
+        end: /^(\s*)(<\/style>)/gm,
+        subLanguage: "stylus",
+        excludeBegin: true,
+        excludeEnd: true,
+      },
+    ],
+  };
+};
--- utils/HtmlRender/Processes/markdown.ts
+++ utils/HtmlRender/Processes/markdown.ts
@@ -1,23 +1,39 @@
 import { Marked } from "marked";
 import { markedHighlight } from "marked-highlight";
 import hljsCore from "highlight.js/lib/core";
+import { hljsDefineVue } from "./hljs-vue";
 
 for (const [alias, languageName] of langAliasAddition.entries()) {
   hljsCore.registerAliases([alias], { languageName });
 }
 
+// 😅
+const langPack = ["xml", "javascript", "typescript", "css", "scss"];
+let isLangPackInstalled = false;
+
 const analyzeAndImport = async (lang: string) => {
+  if (!isLangPackInstalled) {
+    const res = await Promise.all(
+      langPack.map(
+        (lang) =>
+          import(`../../../node_modules/highlight.js/es/languages/${lang}.js`),
+      ),
+    );
+    if (!isLangPackInstalled) {
+      langPack.forEach((each, i) =>
+        hljsCore.registerLanguage(each, res[i].default),
+      );
+      hljsCore.registerLanguage("vue", hljsDefineVue);
+    }
+    isLangPackInstalled = true;
+  }
+  if (hljsCore.getLanguage(lang)) return;
   const target = langAliasMap.get(lang);
   if (target !== undefined) {
     const res = await import(
       `../../../node_modules/highlight.js/es/languages/${target}.js`
     );
     hljsCore.registerLanguage(target, res.default);
-    // JSX语法依赖于XML解析器
-    if (target === "typescript" || target === "javascript") {
-      const res = await import(`highlight.js/lib/languages/xml`);
-      hljsCore.registerLanguage("xml", res.default);
-    }
   } else if (!hljsCore.getLanguage("plaintext")) {
     const res = await import(`highlight.js/lib/languages/plaintext`);
     hljsCore.registerLanguage("plaintext", res.default);
@@ -28,6 +44,7 @@ const marked = new Marked(
     async: true,
     langPrefix: "hljs language-",
     async highlight(code, lang) {
+      lang = lang.toLowerCase();
       await analyzeAndImport(lang);
       const language = hljsCore.getLanguage(lang) ? lang : "plaintext";
       return hljsCore.highlight(code, { language }).value;
