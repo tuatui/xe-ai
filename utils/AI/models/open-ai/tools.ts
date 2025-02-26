9081db895546cf1f2ad3b487f4322d9f0842067b
tuatui <qq2837175561@gmail.com>
2025-02-26 16:14:38 +0800

    fix: 更新提示词并允许更多js工具的权限

--- utils/AI/models/open-ai/tools.ts
+++ utils/AI/models/open-ai/tools.ts
@@ -16,13 +16,14 @@ const JsTools: OpenAI.Chat.Completions.ChatCompletionTool = {
       properties: {
         code: {
           type: "string",
-          description: `要执行的JavaScript 代码，在最后调用\`sendResult2Chat\`以返回代码结果（类似promise的resolve）,或者调用\`sendError2Chat\`返回Error，同时代码会立刻退出。
-          \`sendResult2Chat\`的参数可以为是任何类型，但是必须可以被\`structuredClone\`函数拷贝。
-          例如如果包含回调函数则会报错。如果传入被Proxy代理的对象也会报错，此时建议调用前使用\`JSON.parse(JSON.stringify())\`先处理一遍。
-          \`sendError2Chat\`的参数只能是一个Error的实例。
-          注意：必须在代码结束时调用sendResult2Chat或sendError2Chat，否则代码不会结束并且会导致超时错误。AI助手无法获得console.log warn和 error的输出，而用户只有打开控制台才能看到。
-          代码运行在用户浏览器而并非node.js。
-            `,
+          description: `要执行的JavaScript 代码，在最后调用\`sendResult2Chat\`以返回代码结果（类似promise的resolve）,
+或者调用\`sendError2Chat\`返回Error，同时代码会立刻退出。
+\`sendResult2Chat\`的参数可以为是任何类型，但是必须可以被\`structuredClone\`函数拷贝。
+例如如果包含回调函数则会报错。如果传入被Proxy代理的对象也会报错，此时建议调用前使用\`JSON.parse(JSON.stringify())\`先处理一遍。
+\`sendError2Chat\`的参数只能是一个Error的实例。
+注意：必须在代码结束时调用sendResult2Chat或sendError2Chat（即使参数为空），否则代码不会结束并且会导致超时错误，任何JS调用都必须遵守这一规则。
+AI助手无法获得console.log warn和 error的输出，而用户只有打开控制台才能看到。
+代码运行在用户浏览器而并非node.js。`,
         },
         timeout: {
           type: "number",
@@ -34,7 +35,8 @@ const JsTools: OpenAI.Chat.Completions.ChatCompletionTool = {
     },
   },
 };
-
+const permissionAllow =
+  "accelerometer;ambient-light-sensor;attribution-reporting;autoplay;bluetooth;browsing-topics;camera;compute-pressure;display-capture;encrypted-media;gamepad;geolocation;gyroscope;hid;identity-credentials-get;idle-detection;magnetometer;microphone;midi;otp-credentials;screen-wake-lock;serial;speaker-selection;usb;web-share;window-management;xr-spatial-tracking";
 const jsRun = ({ code, timeout }: { code: string; timeout?: number }) =>
   new Promise<ToolCallReturn>((resolve) => {
     const id = randomDataId();
@@ -73,7 +75,11 @@ const jsRun = ({ code, timeout }: { code: string; timeout?: number }) =>
     // make vue-tsc happy
     // iframe.sandbox = "allow-scripts";
     // error TS2540: Cannot assign to 'sandbox' because it is a read-only property.
-    iframe.setAttribute("sandbox", "allow-scripts");
+    iframe.setAttribute(
+      "sandbox",
+      "allow-scripts allow-downloads allow-modals",
+    );
+    iframe.setAttribute("allow", permissionAllow);
 
     let isDone = false;
     const callback = (ev: MessageEvent) => {
